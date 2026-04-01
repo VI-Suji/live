c10b1bd0e1f83355c6ff6ab6575af9fbc0b37aab
VI-Suji <sujithvi08@gmail.com>
2026-04-01 20:14:16 +0530

    Whatsapp sharing

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -19,33 +19,36 @@ const Meta = ({
 }: MetaProps) => {
     return (
         <Head>
+            {/* Essential Meta Tags for WhatsApp & Social Media */}
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
+            <meta property="og:image:secure_url" content={image} />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
+            <meta property="og:url" content={url} />
+            <meta property="og:type" content={type} />
+            <meta property="og:site_name" content="Gramika News" />
+
             {/* Primary Meta Tags */}
             <title>{title}</title>
             <meta name="title" content={title} />
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
             <meta name="robots" content="index, follow" />
             <meta name="language" content="Malayalam" />
 
             {/* Canonical */}
             <link rel="canonical" href={url} />
 
-            {/* Open Graph / Facebook */}
-            <meta property="og:type" content={type} />
-            <meta property="og:url" content={url} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
-            <meta property="og:site_name" content="Gramika News" />
-            <meta property="og:locale" content="ml_IN" />
-
             {/* Twitter */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content={url} />
-            <meta property="twitter:title" content={title} />
-            <meta property="twitter:description" content={description} />
-            <meta property="twitter:image" content={image} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:url" content={url} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
 
             {/* Favicon */}
             <link rel="icon" href="/gramika.png" type="image/png" />
