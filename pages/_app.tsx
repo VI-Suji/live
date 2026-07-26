c32f3a313d0c8d499a5bad2b4d97c3e0e2d7ec45
VI-Suji <sujithvi08@gmail.com>
2026-07-26 11:40:14 +0530

    Google analytics updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Noto_Sans_Malayalam, Inter } from "next/font/google";
 import LegacyShareRedirect from "@/components/LegacyShareRedirect";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const notoSansMalayalam = Noto_Sans_Malayalam({
   weight: ["400", "500", "600", "700"],
@@ -17,9 +20,52 @@ const inter = Inter({
   display: "swap",
 });
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!GA_MEASUREMENT_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      if (typeof window !== "undefined" && (window as any).gtag) {
+        (window as any).gtag("config", GA_MEASUREMENT_ID, {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <SessionProvider session={session}>
+      {GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
       <div className={`${notoSansMalayalam.variable} ${inter.variable} font-sans`}>
         <LegacyShareRedirect />
         <Component {...pageProps} />
@@ -27,3 +73,4 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     </SessionProvider>
   );
 }
+
